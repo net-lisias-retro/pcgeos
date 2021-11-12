b4395b7f65cb92b069c739d79d9e504ef00b04b0
Falk Rehwagen <frehwagen@bluewaysw.de>
2021-11-12 15:54:55 +0100

    Basic socket driver skeleton added for host API integration. #149

--- Driver/Socket/HstTCPIP/hsttcpipManager.asm
+++ Driver/Socket/HstTCPIP/hsttcpipManager.asm
@@ -63,6 +63,7 @@ include assert.def
 
 include Objects/processC.def
 include medium.def
+include gpmi.def
 
 ;---------------------------------------------------------------------------
 ;				System Libraries
